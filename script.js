cbe8d8bbbd86859d1cf2f8f705cc88738af4e31b
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-11 07:34:21 +0300

    Done with question 3

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Good Luck 💪🏾
+
+
+
+function findMostExpensiveJewelry(jewelry) {
+  let mostExpensiveJewelry = '';
+  let highestPrice = 0;
+
+  for (let i = 0; i < jewelry.length; i++) {
+    let currentJewelry = jewelry[i];
+    let currentPrice = currentJewelry.price;
+
+    if (currentPrice > highestPrice) {
+      highestPrice = currentPrice;
+      mostExpensiveJewelry = currentJewelry.name;
+    }
+  }
+
+  return "The most expensive one is the " + mostExpensiveJewelry + ".";
+}
+
+const jewelry = [
+  { name: 'Sapphires Ring', price: 1600 },
+  { name: 'Gold earrings', price: 1500 },
+  { name: 'Diamond necklace', price: 1400 },
+  { name: 'Emeralds anklet', price: 1200 }
+];
+
+console.log(findMostExpensiveJewelry(jewelry));
